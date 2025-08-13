e56141c2e21e9561ad9436151b683b57cbb901cb
john <johnpesene1992@gmail.com>
2025-08-13 14:58:31 +0100

    mise en jour backend server 700

--- server.js
+++ server.js
@@ -47,8 +47,7 @@ app.get("/",(req,res) =>{
     res.send("API Is Running");
 });
 
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 5000;
 
 
 app.listen(PORT, console.log(`server run in port ${PORT}`));
